ba35422ae4e84284990a939e5872e38f0d4af4fb
fromwhence <wenc.matt@gmail.com>
2020-11-18 11:36:57 -0600

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,42 @@
+// Removes :focus outline for mouse users
+(function(document, window){
+	if (!document || !window) {
+		return;
+	}
+	var styleText = '::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;';
+	var unfocus_style = document.createElement('STYLE');
+
+	window.unfocus = function(){
+		document.getElementsByTagName('HEAD')[0].appendChild(unfocus_style);
+		document.addEventListener('mousedown', function(){
+			unfocus_style.innerHTML = styleText+'}';
+		});
+		document.addEventListener('keydown', function(){
+			unfocus_style.innerHTML = '';
+		});
+	};
+
+	unfocus.style = function(style){
+		styleText += style;
+	};
+
+	unfocus();
+})(document, window);
+
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav-link');
+const mainContent = document.getElementById('main-content');
+
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+})
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+  })
+})
+
+
