86e46d7ec19f8f2e825b479467ee4ef9b4f4f399
fromwhence <wenc.matt@gmail.com>
2020-12-23 15:43:01 -0600

    Added event delegation to main nav

--- js/index.js
+++ js/index.js
@@ -1,43 +1,51 @@
-'use strict';
+"use strict";
 // Removes :focus outline for mouse users
 (function (document, window) {
-	if (!document || !window) {
-		return;
-	}
-	var styleText = '::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;';
-	var unfocus_style = document.createElement('STYLE');
+  if (!document || !window) {
+    return;
+  }
+  var styleText =
+    "::-moz-focus-inner{border:0 !important;}:focus{outline: none !important;";
+  var unfocus_style = document.createElement("STYLE");
 
-	window.unfocus = function () {
-		document.getElementsByTagName('HEAD')[0].appendChild(unfocus_style);
-		document.addEventListener('mousedown', function () {
-			unfocus_style.innerHTML = styleText + '}';
-		});
-		document.addEventListener('keydown', function () {
-			unfocus_style.innerHTML = '';
-		});
-	};
+  window.unfocus = function () {
+    document.getElementsByTagName("HEAD")[0].appendChild(unfocus_style);
+    document.addEventListener("mousedown", function () {
+      unfocus_style.innerHTML = styleText + "}";
+    });
+    document.addEventListener("keydown", function () {
+      unfocus_style.innerHTML = "";
+    });
+  };
 
-	unfocus.style = function (style) {
-		styleText += style;
-	};
+  unfocus.style = function (style) {
+    styleText += style;
+  };
 
-	unfocus();
+  unfocus();
 })(document, window);
 
+const navToggle = document.querySelector(".nav-toggle");
+const navLinks = document.querySelectorAll(".nav-link");
+const mainContent = document.getElementById("main-content");
 
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav-link');
-const mainContent = document.getElementById('main-content');
+navToggle.addEventListener("click", () => {
+  document.body.classList.toggle("nav-open");
+});
 
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.toggle("nav-open");
+  });
+});
 
-navToggle.addEventListener('click', () => {
-	document.body.classList.toggle('nav-open');
-})
-
-navLinks.forEach(link => {
-	link.addEventListener('click', () => {
-		document.body.classList.toggle('nav-open');
-	})
-})
-
-
+// Page navigation using Event Delegation
+document.querySelector(".nav-list").addEventListener("click", function (e) {
+  // e.preventDefault();
+  if (e.target.classList.contains("nav-link")) {
+    console.log("LINK");
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
